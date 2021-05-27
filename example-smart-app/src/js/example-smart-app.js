8732e498648918492b351eb816e559ec6e62d699
Trani, Patrick <84932512+PatCerner@users.noreply.github.com>
2021-05-27 15:05:51 -0500

    auth change
    hope this doesn't break

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,6 +1,11 @@
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
+    ...
+    ...
+    FHIR.oauth2.ready(onReady, onError);
+    return ret.promise();
+  }; 
 
     function onError() {
       console.log('Loading error', arguments);
